c4ecfd85937db65334dc184b4ff32b8b013e1dba
AbdalQader mhemed <abdalqader.mhemed@gmail.com>
2021-05-13 14:30:54 +0300

    add styling and tests

--- src/main/java/com/example/codefellows/control/GeneralControl.java
+++ src/main/java/com/example/codefellows/control/GeneralControl.java
@@ -1,13 +1,28 @@
 package com.example.codefellows.control;
 
+import com.example.codefellows.model.ApplicationUser;
+import com.example.codefellows.repository.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class GeneralControl {
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
 
     @GetMapping("/")
-    public String getSplashPage(){
+    public String getSplashPage(Model model, Principal principal){
+        ApplicationUser user = null;
+        try {
+            user = applicationUserRepository.findByUsername(principal.getName());
+        }catch (Exception e){
+            model.addAttribute("error", e.getMessage());
+        }
+        model.addAttribute("loggedUser", user);
         return "splash.html";
     }
 }
